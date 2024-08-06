c4d0679663c8a53f836bfefbe190710eec5c46a7
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-08-06 21:10:17 +0700

    push source code

--- BackEnd/AppChatBackEnd/AppChatBackEnd/obj/Release/net8.0/AppChatBackEnd.AssemblyInfo.cs
+++ BackEnd/AppChatBackEnd/AppChatBackEnd/obj/Release/net8.0/AppChatBackEnd.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppChatBackEnd")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76f6efbb672475398d59f8c7e17947189f1e7014")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppChatBackEnd")]
