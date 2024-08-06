76f6efbb672475398d59f8c7e17947189f1e7014
luunguyenthanhvu <21130612@st.hcmuaf.edu.vn>
2024-08-06 20:46:02 +0700

    update project

--- BackEnd/AppChat/AppChat/DTO/Request/CreateUserRequestDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AppChat.DTO.Request
-{
-    public class CreateUserRequestDTO
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        public string Email { get; set; }
-    }
-}
