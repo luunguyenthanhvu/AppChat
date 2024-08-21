a45a64baf9e7b4bb668c796465d36f9a1842c3be
NguyenDuyDong862003 <21130318@st.hcmuaf.edu.vn>
2024-08-21 19:09:29 +0700

    Cập nhật front end trang login lấy dc kết quả từ api decode

--- ClientAppChat/app-chat/src/components/Login.js
+++ ClientAppChat/app-chat/src/components/Login.js
@@ -1,33 +1,61 @@
-import React, {createContext, useState, useContext } from 'react';
-import { FaEnvelope, FaLock, FaSignInAlt } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import React, {createContext, useState, useContext} from 'react';
+import {FaEnvelope, FaLock, FaSignInAlt} from 'react-icons/fa';
+import {useNavigate} from 'react-router-dom';
 import '../css/login.css';
-import { BACKEND_URL_HTTP, BACKEND_URL_HTTPS } from '../config.js';
+import {BACKEND_URL_HTTP, BACKEND_URL_HTTPS} from '../config.js';
 import imgHolder from '../img/login-holder.jpg';
 import iconGoogle from '../img/google-icon.png';
 import iconFaceBook from '../img/facebook.png';
-import iconTwitter from '../img/twitter-logo.jpg'; 
+import iconTwitter from '../img/twitter-logo.jpg';
 import Swal from 'sweetalert2';
 import axios from 'axios';
 
-import { GoogleLogin } from '@react-oauth/google';
+import {GoogleLogin} from '@react-oauth/google';
 
 const handleLoginSuccess = (credentialResponse) => {
     console.log(credentialResponse);
-    // Xử lý logic sau khi đăng nhập thành công, ví dụ gửi token đến server.
-    fetch('http://localhost:5133/api/account/google-login', {
+    // gửi token của gg đến api để lấy ra email và username
+
+    fetch('http://localhost:5133/api/LoginGoogle/decode-token-google', {
+        // fetch(`http://${BACKEND_URL_HTTP}/api/LoginGoogle/decode-token-google`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({
-            token: credentialResponse.credential,
-        }),
+        body: JSON.stringify(
+            credentialResponse.credential
+        ),
     })
         .then(response => response.json())
         .then(data => {
-            console.log('Success:', data);
-            // Lưu JWT hoặc thực hiện các bước tiếp theo
+            console.log(data);
+            const email = data.email;
+            const username = data.username;
+            console.log('Email:', email);
+            console.log('Username:', username);
+            // gọi tiếp api: http://localhost:5133/api/LoginGoogle/google-login-response-dto
+            // và truyền vào dto gồm email và username
+            fetch('http://localhost:5133/api/LoginGoogle/google-login-response-dto', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                        email: email,
+                        username: username
+                    }
+                ),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data); // LoginResponseDTO
+                    // chuyển hướng vào app
+
+                })
+                .catch((error) => {
+                    console.error('Error:', error);
+                });
+
         })
         .catch((error) => {
             console.error('Error:', error);
@@ -76,11 +104,11 @@ function Login() {
             try {
                 const response = await axios.post(`http://${BACKEND_URL_HTTP}/api/chat/login`, {
                     email: username,
-                    password : password
+                    password: password
                 });
 
-                    // Giả sử token và các thông tin khác nằm trong response.data
-                const { userName, email, img, token } = response.data;
+                // Giả sử token và các thông tin khác nằm trong response.data
+                const {userName, email, img, token} = response.data;
 
                 localStorage.setItem('userName', userName);
                 localStorage.setItem('email', email);
@@ -97,19 +125,19 @@ function Login() {
                         Swal.showLoading();
                         const timer = Swal.getPopup().querySelector("b");
                         timerInterval = setInterval(() => {
-                        timer.textContent = `${Swal.getTimerLeft()}`;
+                            timer.textContent = `${Swal.getTimerLeft()}`;
                         }, 100);
                     },
                     willClose: () => {
                         clearInterval(timerInterval);
                     }
-                    }).then((result) => {
+                }).then((result) => {
                     if (result.dismiss === Swal.DismissReason.timer) {
                         console.log("I was closed by the timer");
                         navigate('/chat')
                     }
-                    });
-                
+                });
+
             } catch (error) {
                 console.error('Login error:', error);
                 // Xử lý lỗi khi đăng nhập
@@ -119,84 +147,85 @@ function Login() {
     return (
         <div className="background-image">
             <div className='overlay'>
-            <div className='main-container content'>
-            <div className='img-container'>
-                <img src={imgHolder} alt='Login img holder'></img>
-            </div>
-            <div className="login-container">
-                <h2>Login into account</h2>
-                <form onSubmit={loginHandler}>
-                    <div className={`form-group ${isUsernameFocused ? 'focused' : ''}`}>
-                        <label>
-                            <FaEnvelope/> 
-                        </label>
-                        <input
-                            type='text'
-                            value={username}
-                            onChange={(e) => setUsername(e.target.value)}
-                            onFocus={() => setIsUsernameFocused(true)}
-                            onBlur={() => setIsUsernameFocused(false)}
-                            placeholder='Email address'
-                        />
+                <div className='main-container content'>
+                    <div className='img-container'>
+                        <img src={imgHolder} alt='Login img holder'></img>
                     </div>
-                    <div className={`form-group ${isPasswordFocused ? 'focused' : ''}`}>
-                        <label>
-                            <FaLock/>
-                        </label>
-                        <input
-                            type='password'
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            onFocus={() => setIsPasswordFocused(true)}
-                            onBlur={() => setIsPasswordFocused(false)}
-                            placeholder='Password'
-                        />
+                    <div className="login-container">
+                        <h2>Login into account</h2>
+                        <form onSubmit={loginHandler}>
+                            <div className={`form-group ${isUsernameFocused ? 'focused' : ''}`}>
+                                <label>
+                                    <FaEnvelope/>
+                                </label>
+                                <input
+                                    type='text'
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    onFocus={() => setIsUsernameFocused(true)}
+                                    onBlur={() => setIsUsernameFocused(false)}
+                                    placeholder='Email address'
+                                />
+                            </div>
+                            <div className={`form-group ${isPasswordFocused ? 'focused' : ''}`}>
+                                <label>
+                                    <FaLock/>
+                                </label>
+                                <input
+                                    type='password'
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    onFocus={() => setIsPasswordFocused(true)}
+                                    onBlur={() => setIsPasswordFocused(false)}
+                                    placeholder='Password'
+                                />
+                            </div>
+
+                            <button className='login-btn' type='submit'>
+                                LOGIN
+                            </button>
+
+                            <div className='forget-pass'>
+                                <a href=''>
+                                    Forget Password?
+                                </a>
+                            </div>
+
+                            <div>
+                                or login with
+                            </div>
+
+                            <GoogleLogin
+                                onSuccess={handleLoginSuccess}
+                                onError={handleLoginFailure}
+                            />
+
+                            <div className='login-option'>
+                                <a href='' className='icon-login'>
+                                    <img src={iconGoogle}></img>
+                                </a>
+                                <a href='' className='icon-login'>
+                                    <img src={iconFaceBook}></img>
+                                </a>
+                                <a href='' className='icon-login'>
+                                    <img src={iconTwitter}></img>
+                                </a>
+                            </div>
+
+                            <div className='break-line'></div>
+
+                            <div className='register-here'>
+                                Don't have an account?
+                                <a href=''>
+                                    Register here
+                                </a>
+                            </div>
+                        </form>
                     </div>
-
-                    <button className='login-btn' type='submit'>
-                        LOGIN
-                    </button>
-
-                    <div className='forget-pass'>
-                        <a href=''>
-                            Forget Password?
-                        </a>
-                    </div>
-
-                    <div>
-                        or login with
-                    </div>
-
-                    <GoogleLogin
-                        onSuccess={handleLoginSuccess}
-                        onError={handleLoginFailure}
-                    />
-
-                    <div className='login-option'>
-                        <a href='' className='icon-login'>
-                            <img src={iconGoogle}></img>
-                        </a>
-                        <a href='' className='icon-login'>
-                            <img src={iconFaceBook}></img>
-                        </a>
-                        <a href='' className='icon-login'>
-                            <img src={iconTwitter}></img>
-                        </a>
-                    </div>
-
-                    <div className='break-line'></div>
-
-                    <div className='register-here'>
-                        Don't have an account? 
-                        <a href=''>
-                            Register here
-                        </a>
-                    </div>
-                </form>
-            </div>
-            </div>
+                </div>
             </div>
         </div>
     );
 }
+
 export default Login;
