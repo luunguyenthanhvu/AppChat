d2b8d823064317a3a3979ec7a3edc862831906d5
mtuog <119672543+mtuog@users.noreply.github.com>
2024-10-25 19:36:29 +0700

    t3

---
+++ ClientAppChat2/AppChat2/config.js
@@ -0,0 +1,4 @@
+import { API_URL_HTTP, API_URL_HTTPS } from '@env';
+
+export const BACKEND_URL_HTTP = API_URL_HTTP;
+export const BACKEND_URL_HTTPS = API_URL_HTTPS;
---
+++ ClientAppChat2/AppChat2/src/components/AppleStyleSwipeableRow.js
@@ -0,0 +1,96 @@
+import Colors from '../constants/Colors';
+import { Ionicons } from '@expo/vector-icons';
+import React, { Component } from 'react';
+import { Animated, StyleSheet, Text, View, I18nManager } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import Swipeable from 'react-native-gesture-handler/Swipeable';
+
+export default class AppleStyleSwipeableRow extends Component {
+  renderRightAction = (text, color, x, progress) => {
+    const trans = progress.interpolate({
+      inputRange: [0, 1],
+      outputRange: [x, 0],
+    });
+    const pressHandler = () => {
+      this.close();
+      // eslint-disable-next-line no-alert
+      window.alert(text);
+    };
+
+    return (
+      <Animated.View style={{ flex: 1, transform: [{ translateX: trans }] }}>
+        <RectButton style={[styles.rightAction, { backgroundColor: color }]} onPress={pressHandler}>
+          <Ionicons
+            name={text === 'More' ? 'ellipsis-horizontal' : 'archive'}
+            size={24}
+            color={'#fff'}
+            style={{ paddingTop: 10 }}
+          />
+          <Text style={styles.actionText}>{text}</Text>
+        </RectButton>
+      </Animated.View>
+    );
+  };
+
+  renderRightActions = (progress, _dragAnimatedValue) => (
+    <View
+      style={{
+        width: 192,
+        flexDirection: I18nManager.isRTL ? 'row-reverse' : 'row',
+      }}>
+      {this.renderRightAction('More', '#C8C7CD', 192, progress)}
+      {this.renderRightAction('Archive', Colors.muted, 128, progress)}
+    </View>
+  );
+
+  swipeableRow;
+
+  updateRef = ref => {
+    this.swipeableRow = ref;
+  };
+
+  close = () => {
+    if (this.swipeableRow) {
+      this.swipeableRow.close();
+    }
+  };
+
+  render() {
+    const { children } = this.props;
+    return (
+      <Swipeable
+        ref={this.updateRef}
+        friction={2}
+        enableTrackpadTwoFingerGesture
+        rightThreshold={40}
+        renderRightActions={this.renderRightActions}
+        onSwipeableOpen={direction => {
+          console.log(`Opening swipeable from the ${direction}`);
+        }}
+        onSwipeableClose={direction => {
+          console.log(`Closing swipeable to the ${direction}`);
+        }}>
+        {children}
+      </Swipeable>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  leftAction: {
+    flex: 1,
+    backgroundColor: '#497AFC',
+    justifyContent: 'center',
+  },
+  actionText: {
+    color: 'white',
+    fontSize: 16,
+    backgroundColor: 'transparent',
+    padding: 10,
+  },
+  rightAction: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
